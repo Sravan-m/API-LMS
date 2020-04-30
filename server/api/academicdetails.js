84b1736065cbb04a98cef9915693a69cf4f5b96e
ndvssankar <sivashankar@msitprogram.net>
2020-04-30 21:56:04 +0530

    updated comments and code

--- server/api/academicdetails.js
+++ server/api/academicdetails.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json({ limit: '100mb' });
-const Programs = require('../models/programModel');
+const Program = require('../models/programModel');
 const Users = require('../models/userModel');
-const CourseInstances = require('../models/courseInstanceModel');
-const CourseCatalog = require('../models/courseModel');
-const acadDetails = require("../models/academicDetailsModel")
-const axios = require('axios');
+const CInstances = require('../models/courseInstanceModel');
+const CCatalog = require('../models/courseModel');
+const AcadDetails = require("../models/academicDetailsModel")
 const jwt = require('../jwt').jwt;
 const verifyOptions = require('../jwt').jwt.verifyOptions;
 const publicKEY = require('../jwt').publicKEY;
-const getId = require('./user');
 const mongo = require('mongoose');
-var ObjectId = mongo.Types.ObjectId;
+let ObjectId = mongo.Types.ObjectId;
+
 
 /**
  * @api {get} /required/courses/completion/ Request user required course completion status.
@@ -27,151 +25,162 @@ var ObjectId = mongo.Types.ObjectId;
  * @apiFailure {String} specific errors will be returned.
  */
 
-router.get("/required/courses/completion/", async (req, res) => {
-	var userID;
-	
-	try {
-		var accessToken = req.query.token;
-	    var decoded = jwt.verify(accessToken, publicKEY, verifyOptions);
-	    var userID = await Users.findOne({"email":decoded.email});
-	    if (userID === null) {
-	    	res.status(401).send({"error": "Invalid user, Please login again..."});
-	    }
+//  Getting the user based on access token.
+async function getUser(accessToken) {
+    try {
+        console.log(accessToken);
+        const decoded = jwt.verify(accessToken, publicKEY, verifyOptions);
+        const user = await Users.findOne({ "email": decoded.email });
+        return user;
+    } catch(error) {
+        console.log("error: Invalid token");
+    }
+}
 
-	    query =  { "userID" : userID._id };
+router.get("/required/courses/completion/", async (req, res, next) => {
 
-        const data = await acadDetails.findOne(query);
-        if (data === null) {
-        	res.status(401).send({"error":"Student is not enrolled in any of the programs"});
+    try {
+        const user = await getUser(req.query.token);
+
+        query = { "userID": user._id };
+        if (user === null) {
+            return res.status(401).send(
+                { "error": "Invalid user, Please login again..." });
         }
 
-        const prog = await Programs.findOne(data.programID);
-        if (prog === null) {
-        	res.status(401).send({"error":"Student is not enrolled in any of the programs"});
+        //  Getting the students academic details
+        const acadDetails = await AcadDetails.findOne(query);
+        if (acadDetails === null) {
+            return res.status(401).send(
+                { "error": "Student not enrolled in any of the programs" });
         }
 
-        const enrollments = data.enrollments;
+        //  Getting the programs details. This will be used for getting the grade scale.
+        const program = await Program.findOne(acadDetails.programID);
+        if (program === null) {
+            return res.status(401).send(
+                { "error": "Student not enrolled in any of the programs" });
+        }
+
+        //  Getting the students course enrollments of a program
+        const enrollments = acadDetails.enrollments;
         if (enrollments === null) {
-        	res.status(401).send({"error":"Student is not enrolled into any of the courses"});
+            return res.status(401).send(
+                { "error": "Student not enrolled into any of the courses" });
         }
 
+        //  Used to store the course names 
         let courses = [];
+        
+        //  Used to store the grades obtained in the courses enrolled
         let grades = [];
+        
+        //  Used to store the status obtained in the courses enrolled
         let status = [];
+        
+        //  Used to store the points obtained in the courses enrolled
         let points = [];
+        
+        //  Used to store the credits obtained in the courses enrolled
         let credits = [];
 
-        // const numberOfRequiredCourses = getNumberOfRequiredCourses(data.enrollments[0].programID);
-        for (var i = 0; i < enrollments.length; i++) {
-            var courss = enrollments[i].courses;
-			//	The size of the arrays of courseInstances, grades, status should be equal.
-            for (var j = 0; j < courss.length; j++) {
-            	if (courss[j].grades.length !== courss[j].status.length && courss[j].status.length !== courss[j].courseInstances.length) {
-					res.status(401).send({"error": "Grades are not currently available."});
-				} else {
-	            	var courseDetails = await CourseCatalog.findOne(courss[j].courseID);
-					// console.log("Course Name : ", courseDetails.courseName);
-	            	for (var k = 0; k < courss[j].grades.length; k++) {
-						var cinstance = courss[j].grades[k].courseInstance;
-						// console.log(cinstance);
-						var instance = await CourseInstances.findOne({ _id: cinstance });
-						// console.log(instance);
-						if (instance.isCourseRequired === true) {
-							var courseDetails = await CourseCatalog.findOne(courss[j].courseID);
-							// console.log(courseDetails.courseName, courss[j].grades[k].grade);
-							credits.push(instance.numberOfCredits);
-							courses.push(courseDetails.courseName);
-							grades.push(courss[j].grades[k].grade);
-							status.push(courss[j].status[k].status);
-							var idx = prog.gradeScale.findIndex(x => x.grade === courss[j].grades[k].grade);
-							if (idx === -1) {
-								points.push(0);
-							} else {
-								var pts = prog.gradeScale[idx].points;
-								// console.log(idx, prog.gradeScale[idx].points);
-								points.push(prog.gradeScale[idx].points);
-							}
-						} else {
-							break;
-						}
-					}
-        		}
-        	}
+        //  Getting the course details from the course enrollments.
+        for (let i = 0; i < enrollments.length; i++) {
+            let courss = enrollments[i].courses;
+            for (let j = 0; j < courss.length; j++) {
+                if (courss[j].grades.length !== courss[j].status.length && 
+                    courss[j].status.length !== courss[j].courseInstances.length) {
+                    res.status(401).send(
+                        {"status": "Grades are not currently available."});
+                } else {
+                    let courseDetails = await CCatalog.findOne(courss[j].courseID);
+                    for (let k = 0; k < courss[j].grades.length; k++) {
+                        let instance = await CInstances.findOne({ _id: courss[j].grades[k].courseInstance });
+                        if (instance.isCourseRequired === true) {
+                            let courseDetails = await CCatalog.findOne(courss[j].courseID);
+                            credits.push(instance.numberOfCredits);
+                            courses.push(courseDetails.courseName);
+                            grades.push(courss[j].grades[k].grade);
+                            status.push(courss[j].status[k].status);
+
+                            //  Finding the index of the grade in the gradeScale to get the points
+                            const idx = program.gradeScale.findIndex(x => x.grade === courss[j].grades[k].grade);
+                            if (idx === -1) {
+                                points.push(0);
+                            } else {
+                                let pts = program.gradeScale[idx].points;
+                                points.push(program.gradeScale[idx].points);
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
         }
-        let tmpCourses = [];
-        let tmpGrades = [];
-        let tmpStatus = [];
-        let tmpCredits = [];
-        let tmpPoints = [];
+
+        //  Pushing all the information related to a course.
+        let results = [];
+
+        //  Calculating the incomplete courses count
         let incompleteCnt = 0;
-        for (var i = 0; i < courses.length; i++) {
-        	var prevPts = points[i];
-        	var flag = true;
-        	var maxJ = i;
-        	for (var j = i+1; j < courses.length; j++) {
-        		if (courses[i] === courses[j] && prevPts < points[j]) {
-        			maxJ = j;
-        			prevPts = points[j];
-        			flag = false;
-        		}
-        	}
-        	if (flag) {
-	    		tmpCourses.push(courses[maxJ]);
-				tmpStatus.push(status[maxJ]);
-				tmpCredits.push(credits[maxJ]);
-				tmpPoints.push(points[maxJ]);
-				if (grades[maxJ] === "Incomplete") {
-					tmpGrades.push("Incomplete");
-					incompleteCnt += 1;
-				} else if (grades[maxJ] === "none" || 
-							status[maxJ] === "Evaluations in-progress" || 
-		       				status[maxJ] === "Course in-progress" || 
-	        				status[maxJ] === "Registered") {
-						tmpGrades.push("-");
-						incompleteCnt += 1;
-				} else {
-					tmpGrades.push(grades[maxJ]);
-				}
-			}
-        }
 
         let totalCredits = 0;
         let score = 0;
-        for (let i = 0; i < tmpPoints.length; i++) {
-        	score += tmpPoints[i] * tmpCredits[i];
-        	totalCredits += tmpCredits[i];
+
+        for (let i = 0; i < courses.length; i++) {
+            let prevPts = points[i];
+            let flag = true;
+            let maxJ = i;
+
+            //  Finding the best grade index.
+            for (let j = i + 1; j < courses.length; j++) {
+                if (courses[i] === courses[j] && prevPts < points[j]) {
+                    maxJ = j;
+                    prevPts = points[j];
+                    flag = false;
+                }
+            }
+
+            if (flag) {
+                obj = {};
+                obj.courseName = courses[maxJ];
+                obj.status = status[maxJ];
+                obj.credits = credits[maxJ];
+                obj.points = points[maxJ];
+
+                score += points[maxJ] * credits[maxJ];
+                totalCredits += credits[maxJ];
+                
+                if (grades[maxJ] === "Incomplete") {
+                    obj.grade = "Incomplete";
+                    incompleteCnt += 1;
+                } else if (grades[maxJ] === "none" ||
+                    status[maxJ] === "Evaluations in-progress" ||
+                    status[maxJ] === "Course in-progress" ||
+                    status[maxJ] === "Registered") {
+                    obj.grade = "-";
+                    incompleteCnt += 1;
+                } else {
+                    obj.grade = grades[maxJ];
+                }
+                results.push(obj);
+            }
         }
 
-        const isPromoted = (incompleteCnt) ? false : true;
-        
-        let result = { 	"courses": tmpCourses,
-        				"grades": tmpGrades,
-        				"status": tmpStatus,
-        				"points": tmpPoints,
-        				"credits": tmpCredits,
-        				"isPromoted" : isPromoted,
-        				"CGPA": score/totalCredits 	};
-        res.status(200).json(result);
+        const cstatus = (incompleteCnt) ? false : true;
+
+        res.status(200).json({
+            result: results,
+            isPromoted: cstatus,
+            CGPA: score/totalCredits
+        });
+
+        console.log("Sent the Response");
     } catch (error) {
-    	console.log(error);
+        console.log(error);
         res.sendStatus(500);
     }
 });
 
 module.exports = router;
-
-// On success.... the response could be as follows
-// {
-// 	"courses" : [1,2,3,4],
-// 	"grades" : ["a+", -,-,-]
-// 	"status" : ["course completed","registered","inprogress","Incompelete"],
-// 	"overallStatus" : "Welcome to MSIT and you are not registered. Write to help"
-// }
-
-
-// On failure.... the response could be as follows
-// {
-//     "error": "Invalid Token"
-// }
-
-

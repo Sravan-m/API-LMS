bb1f6080e7c8909d93c07693aae4182adde8a388
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2020-05-01 00:50:19 +0530

    fixed bugs and api enhancements

--- server/api/todo.js
+++ server/api/todo.js
@@ -2,32 +2,28 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const mongo = require('mongoose');
-const fs=require("fs")
+const fs = require("fs")
 var ObjectId = mongo.Types.ObjectId;
 const Users = require('../models/userModel');
-const acadDetails=require("../models/academicDetailsModel")
+const acadDetails = require("../models/academicDetailsModel")
 const CourseInstances = require('../models/courseInstanceModel');
 const Content = require("../models/contentModel");
 const Programs = require('../models/programModel');
 const ActivityResponses = require('../models/activityResponseModel');
-const verifyOptions =  require('../jwt').jwt.verifyOptions;
+const verifyOptions = require('../jwt').jwt.verifyOptions;
 const jwt = require('../jwt').jwt;
 const publicKEY = require('../jwt').publicKEY;
-
-const jsonParser = bodyParser.json({ limit: '100mb' });
 const Courses = require('../models/courseModel');
-var multer  = require('multer')
+var multer = require('multer')
 var upload = multer({ dest: './tmp/' })
 
-
-
-router.get('/get-items/', async(req, res) => {
+router.get('/get-items/', async (req, res) => {
     try {
         // console.log('dd');
         var accessToken = req.query.token;
         var decoded = jwt.verify(accessToken, publicKEY, verifyOptions);
-        var userId = await Users.findOne({"email":decoded.email});
-        var query =  {"userID" : userId._id  };
+        var userId = await Users.findOne({ "email": decoded.email });
+        var query = { "userID": userId._id };
         // console.log(query);
         // var user = req.params.user_id;
 
@@ -44,7 +40,7 @@ router.get('/get-items/', async(req, res) => {
                 var cinstance = courses[j].courseInstances[0];
                 var instance = await CourseInstances.find({ _id: cinstance });
                 if (instance[0].isLive) {
-                    var data = await Content.findOne({"courseInstanceID": instance[0]._id })
+                    var data = await Content.findOne({ "courseInstanceID": instance[0]._id })
                     if (data != null) {
                         activities.push(data);
                     }
@@ -52,7 +48,7 @@ router.get('/get-items/', async(req, res) => {
             }
         }
 
-        res.send({activities});
+        res.send({ activities });
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
@@ -60,14 +56,17 @@ router.get('/get-items/', async(req, res) => {
 });
 
 // /api/todo/todo-list
-router.get('/todo-list/', async(req, res) => {
+router.get('/todo-list/', async (req, res) => {
     try {
         // console.log('dd');
         var accessToken = req.query.token;
         var decoded = jwt.verify(accessToken, publicKEY, verifyOptions);
-        var userId = await Users.findOne({"email":decoded.email});
+        var userId = await Users.findOne({ "email": decoded.email });
         // console.log(userId);
-        var query =  {"userID" : new ObjectId(userId._id)  };
+        if (userId == null) {
+        	res.sendStatus(401);
+        }
+        var query = { "userID": new ObjectId(userId._id) };
         var user = userId._id;
 
         // var query = {
@@ -83,74 +82,77 @@ router.get('/todo-list/', async(req, res) => {
                 var courses = enrolments[i].courses;
                 // console.log(courses);
                 for (var j = 0; j < courses.length; j++) {
-                    var cinstance = courses[j].courseInstances[0];
-                    // console.log(cinstance);
                     var coursename = await Courses.find({ _id: courses[j].courseID })
                     var courseName = coursename[0].courseName;
                     // console.log(courseName);
-                    var instance = await CourseInstances.find({ _id: cinstance });
-                    if (instance[0].isLive) {
-                        // console.log(instance);
-                        // console.log(instance[0]._id);
-                        var data = await Content.findOne({"courseInstanceID": instance[0]._id })
-                        // console.log(data);
-                        if (data != null) {
-                            var contentJSON = data.contentJSON;
-                            var courseInstanceID = data.courseInstanceID;
-                            for (var l = 0; l < contentJSON.length; l++) {
-                                var moduleName = contentJSON[l].name;
-                                var moduleId = contentJSON[l].module_id;
-                                var contents = contentJSON[l].content;
-                                // console.log(contents.length);
-                                if (contents != null) {
-                                    for (var p = 0; p < contents.length; p++) {
-                                        var activityName = contents[p].activity_name;
-                                        var activityId = contents[p].activity_id;
-                                        var assignmentType = contents[p].activity_json[0].activityType;
+                    var cinstances = courses[j].courseInstances; // array need to be iterated
+                    // console.log(cinstance);
+                    for(var a = 0; a < cinstances.length; a++) {
+	                    var instance = await CourseInstances.find({ _id: cinstances[a] }); // Iterate all the instances
+	                    // console.log(instance)
+	                    if (instance[0].isLive) {
+	                        // console.log(instance);
+	                        // console.log(instance[0]._id);
+	                        var data = await Content.findOne({ "courseInstanceID": instance[0]._id })
+	                        // console.log(data);
+	                        if (data != null) {
+	                            var contentJSON = data.contentJSON;
+	                            var courseInstanceID = data.courseInstanceID;
+	                            for (var l = 0; l < contentJSON.length; l++) {
+	                                var moduleName = contentJSON[l].name;
+	                                var moduleId = contentJSON[l].module_id;
+	                                var contents = contentJSON[l].content;
+	                                // console.log(contents.length);
+	                                if (contents != null) {
+	                                    for (var p = 0; p < contents.length; p++) {
+	                                        var activityName = contents[p].activity_name;
+	                                        var activityId = contents[p].activity_id;
+	                                        var assignmentType = contents[p].activity_json[0].activityType;
 
-                                        if (assignmentType == "assignment") {
-                                            var etc = contents[p].activity_json[0].dueDate;
-                                            var assignments = {};
+	                                        if (assignmentType == "assignment") {
+	                                            var etc = contents[p].activity_json[0].dueDate;
+	                                            var assignments = {};
 
-                                            console.log(activityName);
-                                            if (etc) {
-                                                assignments["ETC"] = etc;
-                                            } else {
-                                                assignments["ETC"] = "";
-                                            }
-                                            var qres =  {"userId" : user };
-                                            courseInstanceID ? qres["courseInstanceId"] = courseInstanceID:"";
-                                            moduleId ? qres["moduleId"] = moduleId:"";
-                                            activityId ? qres["activityId"] = activityId:"";
-                                            assignmentType ? qres["activityType"] = assignmentType:"";
+	                                            if (etc) {
+	                                                assignments["ETC"] = etc;
+	                                            } else {
+	                                                assignments["ETC"] = "";
+	                                            }
+	                                            var qres = { "userId": user };
+	                                            courseInstanceID ? qres["courseInstanceId"] = courseInstanceID : "";
+	                                            moduleId ? qres["moduleId"] = moduleId : "";
+	                                            activityId ? qres["activityId"] = activityId : "";
+	                                            assignmentType ? qres["activityType"] = assignmentType : "";
 
-                                            const response = await ActivityResponses.find(qres);
-                                            // console.log(response);
-                                            if (response.length == 0) {
-                                                assignments["status"] = false;
-                                            } else {
-                                                assignments["status"] = true;
-                                                // evaluation status 
-                                                // TODO
-                                            }
+	                                            // console.log(activityName);
+	                                            const response = await ActivityResponses.find(qres);
+	                                            // console.log(response);
+	                                            if (response.length == 0) {
+	                                                assignments["status"] = false;
+	                                            } else {
+	                                                assignments["status"] = true;
+	                                                // evaluation status 
+	                                                // TODO
+	                                            }
 
-                                            // assignments["feedback"] = ;
-                                            assignments["courseName"] = courseName;
-                                            assignments["moduleName"] = moduleName;
-                                            assignments["moduleId"] = moduleId;
-                                            assignments["activityName"] = activityName;
-                                            assignments["activityId"] = activityId;
-                                            activities.push(assignments);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+	                                            // assignments["feedback"] = ;
+	                                            assignments["courseName"] = courseName;
+	                                            assignments["moduleName"] = moduleName;
+	                                            assignments["moduleId"] = moduleId;
+	                                            assignments["activityName"] = activityName;
+	                                            assignments["activityId"] = activityId;
+	                                            activities.push(assignments);
+	                                        }
+	                                    }
+	                                }
+	                            }
+	                        }
+	                    }
+	                }
                 }
             }
         }
-        res.send({activities});
+        res.send({ activities });
     } catch (error) {
         console.log("API error");
         res.sendStatus(500);

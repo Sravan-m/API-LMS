bc1d254e3f0900fc7f0fa64d532b04beb383f760
LaxmiNarayanaMurthyVemuri <41734548+LaxmiNarayanaMurthyVemuri@users.noreply.github.com>
2020-05-04 23:59:39 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 const mongo = require('mongoose');
-process.env.MONGO_ATLAS_URL='mongodb+srv://admin:admin@cluster0-fyacg.mongodb.net/test?retryWrites=true&w=majority'
 function startDb() {
   mongo.connect(process.env.MONGO_ATLAS_URL, {
     useNewUrlParser: true
